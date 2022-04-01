58ddbe937a6dd63ab49e960e516def24c017c2ba
pulkit <pulkitrajpal123@gmail.com>
2022-04-01 09:48:53 +0530

    Time: 2 ms (33.86%), Space: 54.7 MB (56.22%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        int len = s.length - 1;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[i];
+            s[i] = s[len - i];
+            s[len - i] = temp;
+        }
+    }
+    }
