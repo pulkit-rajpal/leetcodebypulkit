bc647b86d92ccf17ab391f59deff036596f66486
pulkit <pulkitrajpal123@gmail.com>
2022-04-08 22:11:56 +0530

    Time: 25 ms (34.20%), Space: 53.6 MB (12.63%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,8 +1,15 @@
+/*
+
+1. First point to note is that we have to use offer instead of add .
+2. So we have to find largest in the k element so we can use heap .
+3. we need to add given elements and then build a dd function as we have to find the kth largest element so we will pop till the size become less the k and then pring the peek  */
+
+
 class KthLargest {
 
     final PriorityQueue<Integer> q;
     final  int k;
-    // This function 
+    
     public KthLargest(int k, int[] nums) {
         q= new PriorityQueue<Integer>(k);
         for (int num : nums) {
