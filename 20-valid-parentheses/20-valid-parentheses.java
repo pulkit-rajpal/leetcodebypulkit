6a503db4b24720ec3331d7d0bb7162ef2d483884
pulkit <pulkitrajpal123@gmail.com>
2022-04-28 23:32:20 +0530

    Time: 1 ms (99.41%), Space: 40.1 MB (94.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,43 +1,19 @@
 class Solution {
-
-   
-    private class stack {
-        private int top = 0;
-        private ArrayList<String> data = new ArrayList<String>();
-
-
-        private void push(String val) {
-            data.add(val);
-            top++;
-        }
-
-        private boolean isEmpty() {
-            if (top == 0) return true;
-            return false;
-        }
-
-        private String pop() {
-            top--;
-            String flag = data.remove(top);
-            return flag;
-        }
-    }
-
-
     public boolean isValid(String s) {
-        stack s1 = new stack();
-        String[] list = s.split("");
-        for (String value : list) {
-            if (Objects.equals(value, "(")) {
-                s1.push(")");
-            } else if (Objects.equals(value, "{")) {
-                s1.push("}");
-            } else if (Objects.equals(value, "[")) {
-                s1.push("]");
-            } else if (s1.isEmpty() || !Objects.equals(s1.pop(), value)) {
-                return false;
+      Stack<Character> stack = new Stack();
+        int count = 0;
+        for (char s1 : s.toCharArray()) {
+            if (s1 == '(') {
+                stack.push(')');
+            } else if (s1 == '{') {
+                stack.push('}');
+            } else if (s1 == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != s1) {
+                return  false;
             }
         }
-        return s1.isEmpty();
+        return stack.isEmpty();
+    
     }
 }
